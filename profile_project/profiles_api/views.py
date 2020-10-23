b28717aba0fc67e460770aa505ec034b315affae
Anshul812 <2017eeb1129@iitrpr.ac.in>
2020-10-23 16:26:41 +0530

    fifth commit

--- profile_project/profiles_api/views.py
+++ profile_project/profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self , request , format=None):
+        """Returns a list of API features"""
+        an_apiView = [
+            'Uses Http Methods',
+            'Similar to Django View',
+            'Takes control over url'
+        ]
+
+        return Response({'message':'Hello!' , 'apiView':an_apiView})
